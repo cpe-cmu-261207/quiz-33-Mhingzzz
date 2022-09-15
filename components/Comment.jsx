ff0ed52090e4a57700ef6d1477c44963a475ba79
Mhingzzz <nathaphong_p@cmu.ac.th>
2022-09-15 15:09:00 +0700

    en

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -11,28 +11,34 @@ export default function Comment(props) {
 
 		//   {/*render Reply here... */}
 		// </div>
-		<div className="d-flex gap-2 my-2">
-			<img
-				src={comments[0].userImagePath}
-				width="48"
-				height="48"
-				className="rounded-circle"
-				style={{ objectFit: "cover" }}
-			/>
-			<div
-				className="rounded rounded-3 p-2"
-				style={{ backgroundColor: "#3A3B3C" }}
-			>
-				<span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-					{comments[0].username}
-				</span>
-				<br />
-				<span style={{ color: "#E4E6EB" }}>{comments[0].commentText}</span>
-				<div className="d-flex align-items-center gap-1">
-					<img src="/like.svg" width={20}></img>
-					<span style={{ color: "#B0B3B8" }}>{comments[0].likeNum}</span>
+		<div>
+			{comments.map((comment, i) => (
+				<div>
+					<div className="d-flex gap-2 my-2" key={i}>
+						<img
+							src={comment.userImagePath}
+							width="48"
+							height="48"
+							className="rounded-circle"
+							style={{ objectFit: "cover" }}
+						/>
+						<div
+							className="rounded rounded-3 p-2"
+							style={{ backgroundColor: "#3A3B3C" }}
+						>
+							<span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+								{comment.username}
+							</span>
+							<br />
+							<span style={{ color: "#E4E6EB" }}>{comment.commentText}</span>
+							<div className="d-flex align-items-center gap-1">
+								<img src="/like.svg" width={20}></img>
+								<span style={{ color: "#B0B3B8" }}>{comment.likeNum}</span>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
+			))}
 		</div>
 	);
 }
